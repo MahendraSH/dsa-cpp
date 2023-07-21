d7e28acb29c6b9da8d3c92e2ba69cb232530d152
MahendraSH <maahi2001sh@gmail.com>
2023-07-21 20:28:07 +0530

    create : binary search , create : bs1D

---
+++ basics/BinarySearch/bs1D/pro1.cpp
@@ -0,0 +1,14 @@
+// Problem statement: You are given a sorted array of integers and a target, your task is to search for the target in the given array. Assume the given array does not contain any duplicate numbers.
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> a;
+    for (int i = 0; i < n; i++)
+    {
+        int temp;
+        cin>>temp;
+    }
+}
---
+++ basics/BinarySearch/readme.md
@@ -0,0 +1,5 @@
+# BinarySearch
+
+- ## [BS 1D arrays ](https://github.com/MahendraSH/dsa-cpp/tree/main/basics/BinarySearch/bs1D)
+- ## [BS on ans ](https://github.com/MahendraSH/dsa-cpp/tree/main/basics/BinarySearch/bsAns)
+- ## [BS 2D arrays ](https://github.com/MahendraSH/dsa-cpp/tree/main/basics/BinarySearch/bs2D)
